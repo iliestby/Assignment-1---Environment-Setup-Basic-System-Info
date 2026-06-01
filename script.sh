b769dad32ba3784ab5f73151b9a311896e4422e0
Ilies Tabyaoui <72672350+iliestby@users.noreply.github.com>
2026-06-01 15:04:21 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Assignment 1 - Environment Setup & Basic System Info
+# Name: Ilies TABYAOUI
+# Student ID: 64048
+
+echo "===== Basic System Information ====="
+echo ""
+
+echo "Operating System and Kernel Version:"
+uname -a
+
+echo ""
+
+echo "Current Logged-in User:"
+whoami
+
+echo ""
+
+echo "Current Working Directory:"
+pwd
+
+echo ""
+
+echo "===== End of System Information ====="
